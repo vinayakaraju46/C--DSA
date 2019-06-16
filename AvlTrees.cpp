9e0e2d9efb4e2981e98f8fe6d5aff50b6cc9763a
Captain Winters <30818966+vinayakaraju46@users.noreply.github.com>
2019-06-16 17:28:08 +0530

    Update AvlTrees.cpp

--- AvlTrees.cpp
+++ AvlTrees.cpp
@@ -77,6 +77,97 @@ class AvlTre
     }
 
 
+
+    AvlTre<T> &remove(const T &val)
+    {
+        root = removeNode(val, root);
+    }
+
+    Node<T> *removeNode(const T &val, Node<T> *node)
+    {   Node<T> *tempNode = NULL;
+        if(node == NULL)
+        {
+            return NULL;
+        }
+        else if (val < node->data)
+        {
+            node->left = removeNode(val, node->left);
+        }
+        else if (val > node->data)
+        {
+            node->right = removeNode(val, node->right);
+        }
+        else
+        {   
+            if(node->left == NULL && node->right == NULL)
+            {
+                cout << "Removing the leaf node.." << endl;
+                delete node;
+                return NULL;
+            }
+            else if(node->left != NULL && node->right == NULL)
+            {   cout << "removing the node with one leftchild.." << endl;
+                tempNode = node->left;
+                delete node;
+                return tempNode;
+            }
+            else if(node->left == NULL && node->right != NULL)
+            {   cout << "removing the node with one rightchild" << endl;
+                tempNode = node->right;
+                delete node;
+                return tempNode;
+            }
+            else
+            {
+                cout << "Removing the node with two children.." << endl;
+                tempNode = getPredeccor(node->left);
+                node->data = tempNode->data;
+                node->left = removeNode(tempNode->data,node->left);
+            }
+            
+            
+        }
+        if(node == NULL)
+        {
+            return node;
+        }
+        node->height = max(CalcHeight(node->left),CalcHeight(node->right)) + 1;
+
+        int balance = CalcBalance(node);
+
+        if(balance > 1 && CalcBalance(node->left) >= 0)
+        {
+            return rotateright(node);
+        }
+        else if(balance > 1 && CalcBalance(node->left) < 0)
+        {
+            node->left = rotateright(node->left);
+            return rotateright(node);
+        }
+        else if(balance < -1 && CalcBalance(node->right) <= 0)
+        {
+            return rotateleft(node);
+        }
+        else if(balance < -1 && CalcBalance(node->right) > 0)
+        {
+            node->right = rotateright(node->right);
+            return rotateleft(node);
+        }
+        return node;
+        
+    }
+
+
+    Node<T> *getPredeccor(Node<T> *node)
+    {
+        if(node->right)
+        {
+            return getPredeccor(node->right);
+        }
+        return node;
+    }
+
+
     Node<T> *SettleViolation(const T &val, Node<T> *node)
     {
         int balance = CalcBalance(node);
@@ -143,7 +234,7 @@ class AvlTre
         {
             traverseinorder(node->left);
         }
-        cout << node->data;
+        cout << node->data << endl;
         if(node->right)
         {
             traverseinorder(node->right);
@@ -166,9 +257,12 @@ for(int i=0; i<n; i++)
     cin >> elem;
     avl.insert(elem);
 }
-
+int delem;
 avl.traverse();
-
+cout << "Enter the element to be deleted.." << endl;
+cin >> delem;
+avl.remove(delem);
+avl.traverse();
 
 
 
